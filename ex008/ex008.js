6eaba25df7665c4de5209d3507af77f14b0a31d2
Joao Lucas <joaolucas280708@gmail.com>
2024-08-22 21:30:53 -0300

    fiz os ex 008, 009 e 010

---
+++ ex008/ex008.js
@@ -0,0 +1,7 @@
+var vel = 78
+console.log(`a velocidade do seu carro é ${vel} Km por Hora`)
+if (vel > 60) {
+console.log('você ultrapassou a velocidade permitida, MULTADO!')
+} // condição simples
+
+console.log('dirija sempre usando o cinto de segurança')
