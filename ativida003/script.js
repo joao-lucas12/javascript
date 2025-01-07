6160dc0cbec42aaba3f025f8da135c6506367be3
Joao Lucas <161660533+joao-lucas12@users.noreply.github.com>
2025-01-07 19:22:06 -0300

    fiz o ex003

---
+++ ativida003/script.js
@@ -0,0 +1,33 @@
+function contar() {
+    var i = document.getElementById('txti')
+    var f = document.getElementById('txtf')
+    var p = document.getElementById('txtp')
+    var res = document.getElementById('res')
+    
+    if(i.value.length == 0 || f.value.length == 0 || p.value.length == 0){
+        window.alert('ERRO Confira os dados e tente novamente')
+    } else {
+        res.innerHTML = 'Contando...'
+        let ini = Number(i.value)
+        let fim = Number(f.value)
+        let pas = Number(p.value)
+
+        if(ini < fim){
+            for(let c = ini; c <= fim; c += pas) {
+                res.innerHTML += ` ${c} \u{1F449}`
+    
+            }
+            res.innerHTML += `\u{1F3C1}`
+        } else {
+            for(let c = ini; c >= fim; c -= pas){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+            res.innerHTML += `\u{1F3C1}`
+        }
+
+        }
+
+        
+
+
+}
